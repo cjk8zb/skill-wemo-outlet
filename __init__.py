8ca6a6c5e2cb60c906c6316907bde82e10d80439
Cameron Knight <cameron.knight@moblico.com>
2019-04-08 00:08:14 -0500

    Initital commit

---
+++ __init__.py
@@ -0,0 +1,63 @@
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler, intent_file_handler
+from mycroft.util.parse import match_one, normalize, extract_duration
+
+from mycroft.util.log import getLogger
+from ouimeaux.environment import Environment
+
+
+__author__ = 'cjk8zb'
+
+LOG = getLogger(__name__)
+
+class WemoOutletSkill(MycroftSkill):
+
+    def __init__(self):
+        super(WemoOutletSkill, self).__init__(name="WemoOutletSkill")
+        env = Environment(self.on_switch, self.on_motion)
+        env.start()
+        env.discover(seconds=3)
+
+    @intent_file_handler('change_state.intent')
+    def handle_change_state_intent(self, message):
+        LOG.info("Change state")
+        LOG.info(message.data)
+        match, confidence = match_one(message.data['state'], ['on', 'off'])
+        LOG.info(match)
+        LOG.info(confidence)
+        if confidence < 0.5:
+            return
+
+        button_state = self.switch.get_state()
+
+        if (match == 'on' and button_state == 1) or (match == 'off' and button_state == 0):
+            self.speak('The oven is already ' + match)
+            return
+
+        self.speak('Turning the oven ' + match)
+        new_state = 1 if button_state == 0 else 0
+        self.switch.set_state(new_state)
+
+    @intent_file_handler('running.intent')
+    def handle_running_intent(self, message):
+        LOG.info("running intent")
+        LOG.info(message.data)
+        self.speak('The oven is ' + ('off' if self.switch.get_state() == 0 else 'on'))
+
+    def on_switch(self, switch):
+        print("Switch found!", switch.name)
+        self.switch = switch
+        button_state = self.switch.get_state()
+        print("State:", button_state)
+
+        # new_state = 1 if button_state == 0 else 0
+        # switch.set_state(new_state)
+        # switch.explain()
+
+
+    def on_motion(self, motion):
+        print("Motion found!", motion.name)
+
+
+def create_skill():
+    return WemoOutletSkill()
